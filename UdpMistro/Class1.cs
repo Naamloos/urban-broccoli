c81c251194caa6f662264a517b54bbb6cb33afec
uwx <cadrekucra@gmx.com>
2018-05-20 16:52:23 -0300

    add operation decoder sample

--- UdpMistro/Class1.cs
+++ UdpMistro/Class1.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UdpMistro
 {
-    abstract class UdpBase
+    public abstract class UdpBase
     {
         protected UdpClient Client;
 
@@ -21,16 +24,16 @@ namespace UdpMistro
         {
             var result = await Client.ReceiveAsync();
             var opcode = (Opcode) result.Buffer[0];
-            return new UdpMessage()
+            return new UdpMessage
             {
                 Opcode = opcode,
-                Message = Maestro.DecodeMessage(result.Buffer, 0, result.Buffer.Length),
+                Message = Maestro.DecodeMessage(opcode, result.Buffer, sizeof(byte), result.Buffer.Length),
                 Sender = result.RemoteEndPoint
             };
         }
     }
 
-    internal struct UdpMessage
+    public struct UdpMessage
     {
         public Opcode Opcode;
         public object Message;
@@ -39,15 +42,51 @@ namespace UdpMistro
 
     internal class Maestro
     {
-        private static readonly System.Collections.Immutable.ImmutableDictionary<>
+        private static readonly ImmutableDictionary<Opcode, Func<BinaryReader, IOperation>> Decoders;
 
-        public static object DecodeMessage(byte[] b, int pos, int off)
+        static Maestro()
         {
-            
+            var builder = ImmutableDictionary.CreateBuilder<Opcode, Func<BinaryReader, IOperation>>();
+            builder.Add(Opcode.ClientConnect, ClientConnect.Parse);
+            Decoders = builder.ToImmutable();
+        }
+
+        public static object DecodeMessage(Opcode op, byte[] b, int off, int len)
+        {
+            using (var memoryStream = new MemoryStream(b, off, len))
+            using (var binaryReader = new BinaryReader(memoryStream))
+                return Decoders[op](binaryReader);
         }
     }
 
-    internal enum Opcode : byte
+    public class ClientConnect : IOperation
+    {
+        public ushort Version;
+        public char[] Nick;
+
+        public static ClientConnect Parse(BinaryReader reader)
+        {
+            return new ClientConnect
+            {
+                Version = reader.ReadUInt16(),
+                Nick = reader.ReadChars(reader.ReadUInt16())
+            };
+        }
+
+        public void Write(BinaryWriter writer)
+        {
+            writer.Write(Version);
+            writer.Write((ushort) Nick.Length);
+            writer.Write(Nick);
+        }
+    }
+
+    public interface IOperation
+    {
+        void Write(BinaryWriter writer);
+    }
+
+    public enum Opcode : byte
     {
         // client => server
         ClientConnect, // args: <version: ushort> <nick: string>
